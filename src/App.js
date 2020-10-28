29f0ea0686f0ab17f23f20f45e94a9efb6fbbb59
DerejeKitaw <dereje.kitaw@gmail.com>
2020-10-28 17:16:52 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style.css";
+import {  Form,Button } from 'react-bootstrap'
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+   <div className="container">
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Number of modules</Form.Label>
+        <Form.Control type="text" placeholder="Enter Number of modules" />
+        <Form.Text className="text-muted">
+          Number of omdules in a modules.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+   </div>
   );
 }
